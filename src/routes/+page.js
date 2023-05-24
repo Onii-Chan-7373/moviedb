bf70eeb672e65af53bc3c6c9678752f3faa8a64d
Beppo Alfeo <85755218+Onii-Chan-7373@users.noreply.github.com>
2023-05-24 13:20:35 +0200

    better security
    Improved security when fetching initial data and displaying it

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,10 @@
 export const load = async({fetch}) => {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f23ca6044e548eb28635912d674c26f9`);
+    let res;
+    try{
+        res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f23ca6044e548eb28635912d674c26f9`);
+    } catch(e) {
+        return null;
+    }
     const data = await res.json();
     if(res.ok){
         return {
